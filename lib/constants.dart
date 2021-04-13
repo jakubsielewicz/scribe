1142c179c4ca55287e342a0f68316845790927e9
Jakub Sielewicz <jakub.sielewicz@outlook.com.au>
2021-04-13 22:33:11 +1000

    initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const String appName = 'intelamp.scribe';
+  static const String logoTag = 'intelamp.login.logo';
+  static const String titleTag = 'intelamp.login.title';
+}
